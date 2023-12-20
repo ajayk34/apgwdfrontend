658fbce41057391581d89a07c548cba8159563f2
ajayk34 <208w1a1225@vrsec.ac.in>
2023-12-20 22:33:00 +0530

    ajay

--- src/Jointdirector/Jdmail.jsx
+++ src/Jointdirector/Jdmail.jsx
@@ -1,9 +1,38 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import emailjs from 'emailjs-com';
+
+const fetchallwell = async (userDistrict) => {
+  try {
+    const response = await axios.get('http://localhost:8000/pendinglist', {
+      params: { userDistrict },
+    });
+    console.log(response.data);
+    const extractedValues = response.data.map((item) => `${item.name}`);
+    console.log(extractedValues);
+    const concatenatedString = extractedValues.join('\n');
+    return concatenatedString;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+};
+
+const sendEmail = (email, username, rcount, message) => {
+  emailjs
+    .send('service_c17ljwf', 'template_1r4wb0q', { email, username, rcount, message }, 'cZVSDv5SxvThNvQw0')
+    .then(
+      (result) => {
+        console.log(result.text);
+      },
+      (error) => {
+        console.log(error.text);
+      }
+    );
+};
+
 const Jdmail = () => {
   const [data, setData] = useState([]);
-    const [allwells, setAllwells] = useState('');
+
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -16,46 +45,25 @@ const Jdmail = () => {
 
     fetchData();
   }, []);
-  const fetchallwell = async (userDistrict) => {
-    try {
-      const response = await axios.get('http://localhost:8000/pendinglist',{params:{userDistrict}});
-      console.log(response.data);
-      const extractedValues = response.data.map(item => `${item.name}`);
-        console.log(extractedValues);
-      // Join the extracted values into a single string, separated by a newline character
-      const concatenatedString = extractedValues.join('\n');
-      //  console.log(concatenatedString);
-      // Update the state with the concatenated string 
-        return concatenatedString;
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
-  };
-  const sendEmail = (email,username,rcount, message) => {
-    emailjs
-      .send('service_c17ljwf', 'template_1r4wb0q', { email,username,rcount, message }, 'cZVSDv5SxvThNvQw0')
-      .then(
-        (result) => {
-          console.log(result.text);
-          // You can add further logic here, such as displaying a success message to the user.
-        },
-        (error) => {
-          console.log(error.text);
-          // Handle errors, e.g., display an error message to the user.
-        }
-      );
-  };
 
-
-  const handleSubmit = (username, email,district,rcount) => {
-    // Your submit logic here
-    fetchallwell(district).then((message)=>{
-        sendEmail(email,username,rcount, message);
+  const handleSubmit = (username, email, district, rcount) => {
+    fetchallwell(district).then((message) => {
+      sendEmail(email, username, rcount, message);
     });
+  };
+
+  const sendMailToAll = async () => {
+    // Loop through each item in the data array and send email
+    for (const item of data) {
+      const message = await fetchallwell(item.district);
+      sendEmail(item.email, item.username, item.rcount, message);
+    }
   };
 
   return (
     <div>
+      <h1>Data:</h1>
+      <button onClick={sendMailToAll}>Send Mail to All</button>
       <ul>
         {data.map((item, index) => (
           <li key={index}>
@@ -63,7 +71,9 @@ const Jdmail = () => {
               <p style={{ marginRight: '10px' }}>Username: {item.username}</p>
               <p style={{ marginRight: '10px' }}>RCount: {item.rcount}</p>
               <p>Email: {item.email}</p>
-              <button onClick={() => handleSubmit(item.username, item.email,item.district,item.rcount)}>Send Mail</button>
+              <button onClick={() => handleSubmit(item.username, item.email, item.district, item.rcount)}>
+                Send Mail
+              </button>
             </div>
           </li>
         ))}
